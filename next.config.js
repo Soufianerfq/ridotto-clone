22cbcd6ad4e951dcbeb9eb1104bfef061d169f9e
Soufianerfq <soufianerafiq1999@gmail.com>
2024-11-10 22:47:31 +0100

    trials

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-export const eslint = {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-};
+module.exports = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+}
